6ec851ed79a80ea1cfc0b5b759f77b92d4c37233
fuyfuy8p8 <yakimovvadem@gmail.com>
2022-08-03 22:37:04 +0300

    Commit 1

---
+++ Rise up -/Assets/RiseUp/Common/AdmobRewarded/RewardedVideoButton.cs
@@ -0,0 +1,70 @@
+﻿using GoogleMobileAds.Api;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardedVideoButton : MonoBehaviour
+{
+    private const string ACTION_NAME = "rewarded_video";
+
+    private void Start()
+    {
+        Timer.Schedule(this, 0.1f, AddEvents);
+    }
+
+    private void AddEvents()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        if (AdmobController.instance.rewardBasedVideo != null)
+        {
+            AdmobController.instance.rewardBasedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
+        }
+#endif
+    }
+
+    public void OnClick()
+    {
+        if (IsAvailableToShow())
+        {
+            AdmobController.instance.ShowRewardBasedVideo();
+        }
+        else
+        {
+            Toast.instance.ShowMessage("Ad is not available now, please wait..");
+        }
+        
+        Sound.instance.PlayButton();
+    }
+
+    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
+    {
+
+    }
+
+    public bool IsAvailableToShow()
+    {
+        return IsActionAvailable() && IsAdAvailable();
+    }
+
+    private bool IsActionAvailable()
+    {
+        return CUtils.IsActionAvailable(ACTION_NAME, ConfigController.Config.rewardedVideoPeriod);
+    }
+
+    private bool IsAdAvailable()
+    {
+        if (AdmobController.instance.rewardBasedVideo == null) return false;
+        bool isLoaded = AdmobController.instance.rewardBasedVideo.IsLoaded();
+        return isLoaded;
+    }
+
+    private void OnDestroy()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        if (AdmobController.instance.rewardBasedVideo != null)
+        {
+            AdmobController.instance.rewardBasedVideo.OnAdRewarded -= HandleRewardBasedVideoRewarded;
+        }
+#endif
+    }
+}
---
+++ Rise up -/Assets/RiseUp/Common/AdmobRewarded/RewardedVideoCallBack.cs
@@ -0,0 +1,39 @@
+﻿using GoogleMobileAds.Api;
+using UnityEngine;
+
+public class RewardedVideoCallBack : MonoBehaviour {
+
+    private void Start()
+    {
+        Timer.Schedule(this, 0.1f, AddEvents);
+    }
+
+    private void AddEvents()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        if (AdmobController.instance.rewardBasedVideo != null)
+        {
+            AdmobController.instance.rewardBasedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
+        }
+#endif
+    }
+
+    private const string ACTION_NAME = "rewarded_video";
+    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
+    {
+        int amount = ConfigController.Config.rewardedVideoAmount;
+        CurrencyController.CreditBalance(amount);
+        Toast.instance.ShowMessage("You've received " + amount + " rubies", 3);
+        CUtils.SetActionTime(ACTION_NAME);
+    }
+
+    private void OnDestroy()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        if (AdmobController.instance.rewardBasedVideo != null)
+        {
+            AdmobController.instance.rewardBasedVideo.OnAdRewarded -= HandleRewardBasedVideoRewarded;
+        }
+#endif
+    }
+}
